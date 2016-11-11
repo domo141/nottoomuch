5d57ff584f1236005a385bc76037ba919ed2a12a
Tomi Ollila <too@iki.fi>
2016-11-11 18:52:50 +0200

    Added some devel/ stuff. To be documented.

---
+++ devel/test-in-docker.sh
@@ -0,0 +1,146 @@
+#!/bin/sh
+# -*- mode: shell-script; sh-basic-offset: 8; tab-width: 8 -*-
+# $ test-in-docker.sh $
+
+set -u	# expanding unset variable makes non-interactive shell exit immediately
+set -f	# disable pathname expansion by default -- makes e.g. eval more robust
+set -e	# exit on error -- know potential false negatives and positives !
+#et -x	# s/#/s/ may help debugging  (or run /bin/sh -x ... on command line)
+
+LANG=C LC_ALL=C; export LANG LC_ALL
+PATH='/sbin:/usr/sbin:/bin:/usr/bin'; export PATH
+
+saved_IFS=$IFS; readonly saved_IFS
+
+warn () { printf '%s\n' "$*"; } >&2
+die () { printf '%s\n' "$@"; exit 1; } >&2
+
+known_argvals="'7.11', '8.6', '14.04.5', '16.04', '24' or 'unstable'"
+
+test $# != 0 || die '' "Usage: $0 {debian/ubuntu/fedora base version}" \
+		    '' "version options:" \
+		    "${known_argvals% or *} and ${known_argvals#* or }" ''
+
+case $known_argvals
+  in *"'$1'"*) ;; *) die "'$1' not any of these: $known_argvals".
+esac
+
+# the scheme below works fine with current set of "supported" distributions...
+case $1 in [12]?.04*)	base=ubuntu debian=true
+	;; [23]*)	base=fedora debian=false
+	;; *)		base=debian debian=true
+esac
+
+x () { printf '+ %s\n' "$*" >&2; "$@"; }
+x_env () { printf '+ %s\n' "$*" >&2; env "$@"; }
+x_eval () { printf '+ %s\n' "$*" >&2; eval "$*"; }
+x_exec () { printf '+ %s\n' "$*" >&2; exec "$@"; die "exec '$*' failed"; }
+
+image=notmuch-te-$base-$1
+if test "${2-}" = --in-container--
+then
+	set -x
+	chmod 755 /root # make /root visible to the non-root user...
+	### test -f /etc/bash.bashrc && bashrc=/etc/bash.bashrc || bashrc=/etc/bashrc
+	sed 1,/^---begin-rc---/d "$0" > /etc/profile.d/setuser.sh
+	if $debian
+	then	export DEBIAN_FRONTEND=noninteractive
+		test $1 = 7.11 && emacs=emacs23-nox || emacs=emacs-nox
+		apt-get update
+		apt-get install -y -q build-essential git \
+			libxapian-dev libgmime-2.6-dev libtalloc-dev \
+			zlib1g-dev python-sphinx man dtach $emacs gdb gpgsm
+		apt-get -y autoremove
+		apt-get -y clean
+		rm -rf /var/lib/apt/lists/
+	else
+		: Note: dnf commands below may be long-lasting and silent...
+		dnf -v -y install make gcc gcc-c++ git \
+			xapian-core-devel gmime-devel libtalloc-devel \
+			zlib-devel python2-sphinx man dtach emacs-nox gdb\
+			gnupg2-smime
+		dnf -v -y autoremove
+		dnf -v -y clean all
+	fi
+	exit
+fi
+
+echo Using docker image $image
+
+test "${SUDO_USER-}" && { sudo=sudo user=$SUDO_USER; } || { sudo= user=$USER; }
+
+x docker inspect --type=image --format='{{.ID}}' $image || {
+	# create missing container image
+	from=$base:$1
+	x docker inspect --type=image --format='{{.ID}}' $from ||
+		x docker pull $from
+	x docker create -w /root/.docker-setup \
+		--name wip-$image $from ./"${0##*/}" $1 --in-container-- ||
+		die ": execute; $sudo docker rm wip-$image ;: and try again"
+	x docker cp "$0" wip-$image:/root/.docker-setup/${0##*/}
+	echo Creating docker image $image...
+	x docker start -i wip-$image
+	x docker commit -c "CMD [ \"/bin/echo\", \"start this using '$0'\" ]" \
+	  wip-$image $image
+	x docker rm wip-$image
+}
+
+name=$image-$user
+
+if status=`exec docker inspect -f '{{.State.Status}}' $name 2>&1`
+then
+	if test "$status" = running
+	then x_exec docker exec -it "$name" /bin/bash --login
+	else x_exec docker start -i "$name"
+	fi
+fi
+#run ()
+#{
+	test -d /tmp/.X11-unix &&
+		xv='-v /tmp/.X11-unix:/tmp/.X11-unix:ro' || xv=
+
+	# privileged needed on fedora host for mounts to work
+	sopts='--privileged --ipc=host'
+
+	IFS=:; set -- `exec getent passwd "$user"`
+	home=$6
+	IFS=' '
+	x_exec docker run -it -e DISPLAY -e user="$user" -e home="$home" \
+		--name "$name" -h "$name" -v "$home:$home" \
+		$xv $sopts "$image" /bin/bash --login
+#}
+#case $status in *parsing*error*.State.Status*) run; esac
+
+die error
+
+exit
+---begin-rc---
+
+if test "${UID}" = 0; then
+  if test "${user-}" && test -d "${home:-/kun/ei/ole}"
+  then
+	case $user in *[!-a-z0-9_]*) exit 1; esac
+	grep -q "^$user:" /etc/passwd || {
+		duid=`exec stat -c %u "$home"`
+		useradd -d "$home" -M -u $duid -U -G 0 -s /bin/bash \
+			-c "user $user" "$user" 2>/dev/null || :
+	}
+	echo If root access is desired, docker exec -it non-login shell...
+	# Simple change user which may work as well as gosu(1) if not (better).
+	exec perl -e '
+		my @user = getpwnam $ARGV[0];
+		chdir $user[7];
+		$ENV{HOME} = $user[7];
+		$ENV{USER} = $ARGV[0];
+		delete $ENV{user};
+		$( = $) = "$user[3] $user[3] 0";
+		$< = $> = $user[2]; die "setting uids: $!\n" if $!;
+		exec qw"/bin/bash --login";' "$user"
+  fi
+  echo login shell only for users...
+  exit 1
+fi
+case ${BASH_VERSION-} in *.*)
+	# emulate zsh printexitvalue
+	trap 'echo -n bash: exit $? \ \ ; fc -nl -1 -1' ERR
+esac
---
+++ devel/test-on-macos.sh
@@ -0,0 +1,105 @@
+#!/bin/sh
+
+set -euf
+
+# tput: unknown terminal "rxvt-unicode"
+term=$TERM
+export TERM=xterm
+
+die () { printf '%s\n' "$@"; exit 1; } >&2
+x () { printf '+ %s\n' "$*" >&2; "$@"; }
+x_eval () { printf '+ %s\n' "$*" >&2; eval "$*"; }
+
+test "`exec uname -s`" = Darwin || die "uname -s: '`uname -s`' not 'Darwin'"
+
+BREWLOC=$HOME/.local/share/brew
+case ${BREWLOC} in *["$IFS"]*) die whitespace ;; */) die 'trailing /' ;; esac
+
+case $PATH in *$BREWLOC/*)
+	echo '' $BREWLOC/ already in
+	echo '' $PATH
+	ps -xfp ${PPID:+$PPID,}$$
+	echo '' continuing to use current shell environment "($SHELL $$)"
+	exit
+esac
+
+test -d ${BREWLOC} || die '' "Directory '${BREWLOC}' nonexistent." '' \
+	'This system installs (home)brew in abovementioned location.' \
+	'If you want to use "standard" location just install the few brew' \
+	"packages listed by ; grep install_if_missing '$0'" \
+	": else execute ; mkdir -p ${BREWLOC} ;: to continue" ''
+
+missing=''
+for cmd in gcc g++
+do
+	command -v $cmd >/dev/null || missing=${missing:+$missing }$cmd
+done
+test -z "$missing" || die "Required commands missing: $missing (XCode?)"
+unset missing cmd
+
+test -x ${BREWLOC}/bin/brew || {
+#	x mkdir -p ${BREWLOC%/*} # parent dir
+#	x mkdir ${BREWLOC} ;: if this fails, stop '(or rm -rf and retry)'
+
+	x curl -L https://github.com/Homebrew/brew/tarball/master \
+		| x bsdtar -C ${BREWLOC} --strip-components 1 -xvf -
+}
+
+#mkdir -p $HOME/bin
+# XXX not with brewloc as currently in below
+#ln -s ../.local/share/brew/bin/brew $HOME/bin
+
+install_if_missing ()
+{
+	test -d ${BREWLOC}/Cellar/$1 || x ${BREWLOC}/bin/brew install $1
+}
+
+# cli & lib
+install_if_missing xapian
+install_if_missing gmime
+install_if_missing talloc
+
+# emacs
+install_if_missing emacs
+
+## doc
+# #install_if_missing xxx-sphinx-xxx ## xxx needs pip to install
+
+# bash 4.x for testing
+install_if_missing bash
+
+# dtach(1) for testing
+install_if_missing dtach
+
+# gdb(1) & gpgsm for testing
+install_if_missing gdb
+install_if_missing gpgme
+
+
+# note: with 'export', 'x' works. without it would not work (x_eval would then)
+case $PATH in */brew/bin/*) ;;
+	*) x export 'PATH'="$HOME/.local/share/brew/bin:$PATH"
+esac
+case ${CPATH-} in */brew/include/*) ;;
+	*) x export 'CPATH'="$HOME/.local/share/brew/include${CPATH:+:$CPATH}"
+esac
+LB=${LIBRARY_PATH-}
+case $LB in */brew/lib/*) ;;
+	*) x export 'LIBRARY_PATH'="$HOME/.local/share/brew/lib${LB:+:$LB}"
+esac
+## XXX does not wrok
+#LB=${DYLD_LIBRARY_PATH-}
+#case $LB in */brew/lib/*) ;;
+# *) x export 'DYLD_LIBRARY_PATH'="$HOME/.local/share/brew/lib${LB:+:$LB}"
+#esac
+
+echo
+echo Starting interactive shell with modified environment.
+echo
+echo DYLD_* variables no longer work in macOS '(sierra, el capitan)'.
+echo : Use ./configure --prefix=\$PWD ';:' before testing
+echo : and ./configure --prefix=$HOME/.local/share/brew ';:' before installing.
+echo : '(and 4 testing CDLL("../../libnotmuch.{0:s}.dylib".format(SOVERSION)))'
+echo
+export 'TERM'="$TERM"
+x exec ${SHELL} -i
